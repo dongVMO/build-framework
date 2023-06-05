7d8a927594123198d84e48c11b72fca566bdf5ba
dongVMO <dongov@vmodev.com>
2023-06-05 11:07:06 +0700

    post report to reportPortal page remote

--- src/test/java/saucedemo/runners/RunCucumberTest.java
+++ src/test/java/saucedemo/runners/RunCucumberTest.java
@@ -4,11 +4,14 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
+        plugin = {"pretty", "com.epam.reportportal.cucumber.ScenarioReporter"},
         features = "src/test/resources/saucedemo/features",
         glue = "core.saucedemo.actions.stepdefinitions",
 //        dryRun = false,
         monochrome = true,
-        tags = "@login"
+        tags = "@login",
+        strict = true
+
 )
 public class RunCucumberTest extends AbstractTestNGCucumberTests {
 }
